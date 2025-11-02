dcb3580f2fbcc6b7d4a2efeb878fae447bc6968c
VivPrime <2277921441@qq.com>
2025-11-02 11:50:45 +0800

    模版

--- Hardware/Encoder.c
+++ Hardware/Encoder.c
@@ -1,64 +1,65 @@
-#include "stm32f10x.h"                  // Device header
+#include "Encoder.h"
 
-/**
-  * 函    数：编码器初始化
-  * 参    数：无
-  * 返 回 值：无
-  */
-void Encoder_Init(void)
-{
-	/*开启时钟*/
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);			//开启TIM3的时钟
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);			//开启GPIOA的时钟
-	
-	/*GPIO初始化*/
-	GPIO_InitTypeDef GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);							//将PA6和PA7引脚初始化为上拉输入
-		
-	/*时基单元初始化*/
-	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;				//定义结构体变量
-	TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;     //时钟分频，选择不分频，此参数用于配置滤波器时钟，不影响时基单元功能
-	TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up; //计数器模式，选择向上计数
-	TIM_TimeBaseInitStructure.TIM_Period = 65536 - 1;               //计数周期，即ARR的值
-	TIM_TimeBaseInitStructure.TIM_Prescaler = 1 - 1;                //预分频器，即PSC的值
-	TIM_TimeBaseInitStructure.TIM_RepetitionCounter = 0;            //重复计数器，高级定时器才会用到
-	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseInitStructure);             //将结构体变量交给TIM_TimeBaseInit，配置TIM3的时基单元
-	
-	/*输入捕获初始化*/
-	TIM_ICInitTypeDef TIM_ICInitStructure;							//定义结构体变量
-	TIM_ICStructInit(&TIM_ICInitStructure);                         //结构体初始化，若结构体没有完整赋值
-	                                                                //则最好执行此函数，给结构体所有成员都赋一个默认值
-	                                                                //避免结构体初值不确定的问题
-	TIM_ICInitStructure.TIM_Channel = TIM_Channel_1;                //选择配置定时器通道1
-	TIM_ICInitStructure.TIM_ICFilter = 0xF;                         //输入滤波器参数，可以过滤信号抖动
-	TIM_ICInit(TIM3, &TIM_ICInitStructure);                         //将结构体变量交给TIM_ICInit，配置TIM3的输入捕获通道
-	TIM_ICInitStructure.TIM_Channel = TIM_Channel_2;                //选择配置定时器通道2
-	TIM_ICInitStructure.TIM_ICFilter = 0xF;                         //输入滤波器参数，可以过滤信号抖动
-	TIM_ICInit(TIM3, &TIM_ICInitStructure);                         //将结构体变量交给TIM_ICInit，配置TIM3的输入捕获通道
-	
-	/*编码器接口配置*/
-	TIM_EncoderInterfaceConfig(TIM3, TIM_EncoderMode_TI12, TIM_ICPolarity_Rising, TIM_ICPolarity_Falling);
-																	//配置编码器模式以及两个输入通道是否反相
-	                                                                //注意此时参数的Rising和Falling已经不代表上升沿和下降沿了，而是代表是否反相
-	                                                                //此函数必须在输入捕获初始化之后进行，否则输入捕获的配置会覆盖此函数的部分配置
-	
-	/*TIM使能*/
-	TIM_Cmd(TIM3, ENABLE);			//使能TIM3，定时器开始运行
+TIM_HandleTypeDef htim2;  // 编码器定时器
+int32_t encoder_position = 0;
+
+void Encoder_Init(void) {
+    TIM_Encoder_InitTypeDef encoder_config = {0};
+    TIM_MasterConfigTypeDef master_config = {0};
+    GPIO_InitTypeDef gpio_init = {0};
+    
+    // 时钟使能
+    __HAL_RCC_TIM2_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    
+    // GPIO配置 - PA0, PA1 (TIM2_CH1, TIM2_CH2)
+    gpio_init.Pin = GPIO_PIN_0 | GPIO_PIN_1;
+    gpio_init.Mode = GPIO_MODE_INPUT;
+    gpio_init.Pull = GPIO_PULLUP;
+    HAL_GPIO_Init(GPIOA, &gpio_init);
+    
+    // 定时器基础配置
+    htim2.Instance = TIM2;
+    htim2.Init.Prescaler = 0;
+    htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+    htim2.Init.Period = 0xFFFF;
+    htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+    
+    // 编码器模式配置
+    encoder_config.EncoderMode = TIM_ENCODERMODE_TI12;
+    encoder_config.IC1Polarity = TIM_ICPOLARITY_RISING;
+    encoder_config.IC2Polarity = TIM_ICPOLARITY_RISING;
+    encoder_config.IC1Selection = TIM_ICSELECTION_DIRECTTI;
+    encoder_config.IC2Selection = TIM_ICSELECTION_DIRECTTI;
+    encoder_config.IC1Prescaler = TIM_ICPSC_DIV1;
+    encoder_config.IC2Prescaler = TIM_ICPSC_DIV1;
+    encoder_config.IC1Filter = 0;
+    encoder_config.IC2Filter = 0;
+    
+    HAL_TIM_Encoder_Init(&htim2, &encoder_config);
+    
+    // 主模式配置
+    master_config.MasterOutputTrigger = TIM_TRGO_RESET;
+    master_config.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+    HAL_TIMEx_MasterConfigSynchronization(&htim2, &master_config);
+    
+    // 启动编码器
+    HAL_TIM_Encoder_Start(&htim2, TIM_CHANNEL_ALL);
 }
 
-/**
-  * 函    数：获取编码器的增量值
-  * 参    数：无
-  * 返 回 值：自上此调用此函数后，编码器的增量值
-  */
-int16_t Encoder_Get(void)
-{
-	/*使用Temp变量作为中继，目的是返回CNT后将其清零*/
-	int16_t Temp;
-	Temp = TIM_GetCounter(TIM3);
-	TIM_SetCounter(TIM3, 0);
-	return Temp;
+int16_t Encoder_GetSpeed(void) {
+    static int16_t last_count = 0;
+    int16_t current_count = (int16_t)TIM2->CNT;
+    int16_t speed = current_count - last_count;
+    last_count = current_count;
+    return speed;
+}
+
+int32_t Encoder_GetPosition(void) {
+    return encoder_position;
+}
+
+void Encoder_ClearPosition(void) {
+    encoder_position = 0;
+    __HAL_TIM_SET_COUNTER(&htim2, 0);
 }
